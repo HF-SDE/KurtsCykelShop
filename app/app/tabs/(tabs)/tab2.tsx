8c6d5115ef8788cafce961d8f6f171ebbd3e2d3d
Kenni Kollemorten <kenn7575@gmail.com>
2026-02-17 14:04:09 +0100

    feat: added gluestack UI and setup basic theme

---
+++ app/app/tabs/(tabs)/tab2.tsx
@@ -0,0 +1,11 @@
+
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+export default function Tab2() {
+  return (
+    <View className="flex-1 bg-background-0">
+      <Text className="p-4 text-typography-900">Example below to use gluestack-ui components.</Text>
+    </View>
+  );
+}
