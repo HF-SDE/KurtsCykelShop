201773a4394800e104b2caa69cadbeac997b113d
Phiko <phgu03@gmail.com>
2026-02-18 11:58:00 +0100

    ✨ feat(app.config): Update expo-secure-store configuration for Android backup

--- app/app.config.ts
+++ app/app.config.ts
@@ -41,7 +41,12 @@ const config: ExpoConfig = {
         },
       },
     ],
-    "expo-secure-store",
+    [
+      "expo-secure-store",
+      {
+        configureAndroidBackup: true,
+      },
+    ],
   ],
   experiments: {
     typedRoutes: true,
