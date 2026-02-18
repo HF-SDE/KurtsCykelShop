b494e5c4545786b256b01ff3fc554a90c476a46c
Phiko <phgu03@gmail.com>
2026-02-18 11:58:20 +0100

    🐳 chore(metro.config): Enhance configuration for nativewind and svg support

--- app/metro.config.js
+++ app/metro.config.js
@@ -3,4 +3,20 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+}, { input: './global.css' });
