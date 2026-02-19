e354d80194688354b88b210fec31a310c8c74646
Nima Badiei <nimabadiei@gmail.com>
2026-02-19 12:46:09 +0100

    ✨ feat(storage): Refactor storage module with new layout, barcode scanner, and item management features

---
+++ app/app/(auth)/(tabs)/storage/new-item.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@components/ui/box";
+import { Text } from "@components/ui/text";
+
+export default function NewItem() {
+  return (
+    <Box className="bg-background-0 flex-1">
+      <Text className="mt-10 text-center text-2xl font-bold">New Item</Text>
+    </Box>
+  );
+}
