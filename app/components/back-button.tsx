ca2513d173e666ebd978cb522a2e295ce6e479b8
Phiko <phgu03@gmail.com>
2026-02-23 08:41:46 +0100

    ✨ feat(back-button): Refactor BackButtonLayout for improved layout and structure

--- app/components/back-button.tsx
+++ app/components/back-button.tsx
@@ -2,6 +2,7 @@ import { Box } from "@components/ui/box";
 import { Button, ButtonIcon } from "@components/ui/button";
 import { Stack, useRouter } from "expo-router";
 import { ChevronLeft } from "lucide-react-native";
+import { Platform, View } from "react-native";
 
 export function BackButtonLayout() {
   const router = useRouter();
@@ -12,15 +13,20 @@ export function BackButtonLayout() {
         headerShown: true,
         headerBackVisible: false,
         headerTitle: "",
-        headerBackground: () => <Box className="bg-background-0 flex-1" />,
+        headerBackground: () => <Box className="bg-background-0" />,
         headerLeft: () => (
-          <Button onPress={() => router.back()} size="4xl" action="secondary" variant="link">
-            <ButtonIcon as={ChevronLeft} />
-          </Button>
+          <View style={{ marginTop: Platform.OS === "android" ? -20 : 0 }}>
+            <Button onPress={() => router.back()} size="4xl" action="secondary" variant="link" className="h-10 w-10">
+              <ButtonIcon as={ChevronLeft} />
+            </Button>
+          </View>
         ),
       }}
     >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
     </Stack>
   );
 }
