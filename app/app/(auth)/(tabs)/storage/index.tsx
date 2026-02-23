07abc92c57056f899dce44d58506ac43263356d5
Nima Badiei <nimabadiei@gmail.com>
2026-02-23 11:48:47 +0100

    ✨ feat(storage): integrate dynamic item loading and improve UI for empty state

--- app/app/(auth)/(tabs)/storage/index.tsx
+++ app/app/(auth)/(tabs)/storage/index.tsx
@@ -2,28 +2,26 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Table, TableBody, TableData, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
+import { Item } from "@/types/Inventory/Item";
+
 import { NavigationButton } from "@components/navigation-button";
 import { Searchbar } from "@components/search";
 import { Box } from "@components/ui/box";
-import { Button, ButtonGroup, ButtonIcon } from "@components/ui/button";
+import { Button, ButtonGroup, ButtonIcon, ButtonText } from "@components/ui/button";
+import { Text } from "@components/ui/text";
+import { useData } from "@hooks/useData";
 import { ListFilter, Pencil, Plus, ScanText } from "lucide-react-native";
 
-const data = [
-  { name: "John Doe", quantity: 2 },
-  { name: "Foo Bar", quantity: 6 },
-  { name: "Ben Dover", quantity: 7 },
-  { name: "John Doe", quantity: 2 },
-  { name: "Foo Bar", quantity: 6 },
-  { name: "Ben Dover", quantity: 7 },
-  { name: "John Doe", quantity: 2 },
-  { name: "Foo Bar", quantity: 6 },
-  { name: "Ben Dover", quantity: 7 },
-  { name: "John Doe", quantity: 2 },
-  { name: "Foo Bar", quantity: 6 },
-  { name: "Ben Dover", quantity: 7 },
-];
-
 export default function Storage() {
+  const [items, , isLoading] = useData<Item>("/items");
+
+  if (isLoading)
+    return (
+      <Box className="bg-background-0 flex-1">
+        <Text>Loading...</Text>
+      </Box>
+    );
+
   return (
     <SafeAreaView className="bg-background-0 w-full flex-1 overflow-hidden p-2">
       <Box className="mb-8 w-full flex-row justify-between">
@@ -46,34 +44,47 @@ export default function Storage() {
         </ButtonGroup>
       </Box>
 
-      <Table className="w-full">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Quantity</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {data.map((item, i) => (
-            <TableRow key={i}>
-              <TableData>{item.name}</TableData>
-              <TableData>{item.quantity}</TableData>
-              <TableData>
-                <NavigationButton
-                  variant="outline"
-                  action="secondary"
-                  className="!border-0"
-                  href={`/storage/${i}/edit-item`}
-                >
-                  <ButtonIcon size="3xl" as={Pencil} />
-                </NavigationButton>
-              </TableData>
+      {items.length ? (
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Action</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+
+          <TableBody>
+            {items.map((item, i) => (
+              <TableRow key={i}>
+                <TableData>{item.name}</TableData>
+                <TableData>{item.quantity}</TableData>
+                <TableData>
+                  <NavigationButton
+                    variant="outline"
+                    action="secondary"
+                    className="!border-0"
+                    href={`/storage/${i}/edit-item`}
+                  >
+                    <ButtonIcon size="3xl" as={Pencil} />
+                  </NavigationButton>
+                </TableData>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <Box className="bg-background-0 flex-1 items-center justify-center">
+          <Text size="lg" className="mb-4">
+            No items found
+          </Text>
+
+          <NavigationButton href="/storage/new-item" variant="outline" action="secondary" size="lg">
+            <ButtonIcon as={Plus} />
+            <ButtonText>Add your first item</ButtonText>
+          </NavigationButton>
+        </Box>
+      )}
     </SafeAreaView>
   );
 }
