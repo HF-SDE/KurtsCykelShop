36703c3bb55ea4129bd4356334e09f559469093f
Kenni Kollemorten <kenn7575@gmail.com>
2026-02-23 09:13:25 +0100

    fix: updated api response type to match backend

--- app/utils/ApiResponse.ts
+++ app/utils/ApiResponse.ts
@@ -1,5 +1,24 @@
-export interface ApiResponse<T> {
-  data: T;
+export interface APIResponse<T = null | undefined> {
+  status: Status;
   message?: string;
-  status?: string;
+  data?: T | null;
+}
+export enum Status {
+  Unauthorized = "Unauthorized",
+  Forbidden = "Forbidden",
+  Success = "Success",
+  Failed = "Failed",
+  Found = "Found",
+  NotFound = "NotFound",
+  Created = "Created",
+  CreationFailed = "CreationFailed",
+  Deleted = "Deleted",
+  DeleteFailed = "DeletionFailed",
+  Updated = "Updated",
+  UpdateFailed = "UpdateFailed",
+  MissingDetails = "MissingDetails",
+  InvalidDetails = "InvalidDetails",
+  MissingCredentials = "MissingCredentials",
+  InvalidCredentials = "InvalidCredentials",
+  TooManyRequests = "TooManyRequests",
 }
