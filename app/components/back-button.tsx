00ab7a4f04fb9ca6f33e7e377da2f689a7857806
Phiko <phgu03@gmail.com>
2026-02-23 14:15:07 +0100

    🐞 fix(back-button): enable back button visibility in header options

--- app/components/back-button.tsx
+++ app/components/back-button.tsx
@@ -5,33 +5,33 @@ import { Button, ButtonIcon } from "@components/ui/button";
 import { Stack, useRouter } from "expo-router";
 import { ChevronLeft } from "lucide-react-native";
 
-export function BackButtonLayout({ children }: { children?: React.ReactNode }) {
+export function BackButtonLayout() {
   const router = useRouter();
 
   return (
     <Stack
       screenOptions={{
         headerShown: true,
-        headerBackVisible: false,
+        headerBackVisible: true,
+        // headerStyle: { backgroundColor: "transparent" },
         headerTitle: "",
-        headerBackground: () => <Box className="bg-background-0 flex-1" />,
-        headerLeft: () => (
-          <View style={{ marginTop: Platform.OS === "android" ? -20 : 0 }}>
-            <Button
-              onPress={() => router.back()}
-              size="4xl"
-              action="secondary"
-              variant="link"
-              className="h-10 w-10 !border-0"
-            >
-              <ButtonIcon as={ChevronLeft} />
-            </Button>
-          </View>
-        ),
+        headerBackground: () => <Box className="bg-background-0 flex-1" style={{ height: "100%" }} />,
+        // headerLeft: () => (
+        //   <View>
+        //     <Button
+        //       onPress={() => router.back()}
+        //       size="4xl"
+        //       action="secondary"
+        //       variant="link"
+        //       className="h-10 w-10 !border-0"
+        //     >
+        //       <ButtonIcon as={ChevronLeft} />
+        //     </Button>
+        //   </View>
+        // ),
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      {children}
     </Stack>
   );
 }
