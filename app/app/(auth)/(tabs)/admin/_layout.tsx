d823ffd6c4303e1d5324135bb0f225e2eb1ed41a
Phiko <phgu03@gmail.com>
2026-02-23 08:41:51 +0100

    ✨ feat(storage-layout): Simplify StorageLayout by using BackButtonLayout component

--- app/app/(auth)/(tabs)/admin/_layout.tsx
+++ app/app/(auth)/(tabs)/admin/_layout.tsx
@@ -1,26 +1,5 @@
-import { Box } from "@components/ui/box";
-import { Button, ButtonIcon } from "@components/ui/button";
-import { Stack, useRouter } from "expo-router";
-import { ArrowLeft } from "lucide-react-native";
+import { BackButtonLayout } from "@components/back-button";
 
 export default function StorageLayout() {
-  const router = useRouter();
-
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: true,
-        headerBackVisible: false,
-        headerTitle: "",
-        headerBackground: () => <Box className="bg-background-0 flex-1" />,
-        headerLeft: () => (
-          <Button onPress={() => router.back()} size="3xl" className="p-3" variant="link">
-            <ButtonIcon as={ArrowLeft} />
-          </Button>
-        ),
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <BackButtonLayout />;
 }
