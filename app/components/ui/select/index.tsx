39912726e755749f1e7b2f02185cbde7893b27e7
Nima Badiei <nimabadiei@gmail.com>
2026-02-23 12:55:05 +0100

    fix(imports): consolidate imports from nativewind-utils for cleaner code

--- app/components/ui/select/index.tsx
+++ app/components/ui/select/index.tsx
@@ -5,8 +5,7 @@ import { Pressable, TextInput, View } from "react-native";
 
 import { PrimitiveIcon, UIIcon } from "@gluestack-ui/core/icon/creator";
 import { createSelect } from "@gluestack-ui/core/select/creator";
-import { tva } from "@gluestack-ui/utils/nativewind-utils";
-import { useStyleContext, withStyleContext } from "@gluestack-ui/utils/nativewind-utils";
+import { tva, useStyleContext, withStyleContext } from "@gluestack-ui/utils/nativewind-utils";
 import type { VariantProps } from "@gluestack-ui/utils/nativewind-utils";
 import { cssInterop } from "nativewind";
 
