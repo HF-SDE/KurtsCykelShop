06181c4579b7d802f1497e1ae131168a4b070a2e
Phiko <phgu03@gmail.com>
2026-02-24 10:21:14 +0100

    ✨ feat(layout): update AdminLayout to include roles screen and enhance StorageLayout with initial route settings

--- app/app/(auth)/(tabs)/admin/_layout.tsx
+++ app/app/(auth)/(tabs)/admin/_layout.tsx
@@ -1,5 +1,10 @@
 import { BackButtonLayout } from "@components/back-button";
+import { Stack } from "expo-router";
 
-export default function StorageLayout() {
-  return <BackButtonLayout />;
+export default function AdminLayout() {
+  return (
+    <BackButtonLayout>
+      <Stack.Screen name="roles" options={{ headerShown: false }} />
+    </BackButtonLayout>
+  );
 }
