aa17ebb2ed27805e9d8c6c8275afc4bb2bc1388b
Phiko <phgu03@gmail.com>
2026-02-24 13:27:05 +0100

    ✨ feat(routes): add role management routes with permissions for viewing, creating, and updating roles

--- backend/src/api/routes/manage.routes.ts
+++ backend/src/api/routes/manage.routes.ts
@@ -6,6 +6,7 @@ import {
   patchUser,
   updateUser,
 } from "@controllers/manage.controller";
+import { createRole, getRole, getRoles, updateRole } from "@controllers/role.controller";
 import { verifyJWT } from "@middlewares/authenticate.mw";
 import { isAllowed } from "@middlewares/isAllowed.mw";
 import { validateParams } from "@middlewares/validate.mw";
@@ -27,5 +28,10 @@ router.get("/permissionGroups", isAllowed(["administrator:permission:view"]), va
 router.post("/user", isAllowed(["administrator:users:create"]), createUser);
 router.put("/user/:id", isAllowed(["administrator:users:update"]), updateUser);
 router.patch("/user/:id", isAllowed(["administrator:users:update"]), patchUser);
+
+router.get("/roles", isAllowed(["administrator:roles:view"]), getRoles);
+router.get("/roles/:id", isAllowed(["administrator:roles:view"]), validateParams, getRole);
+router.post("/roles", isAllowed(["administrator:roles:create"]), createRole);
+router.put("/roles/:id", isAllowed(["administrator:roles:update"]), updateRole);
 
 export default router;
