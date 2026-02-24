ed8050f82e22d9b7f1e6950d4cd8c60ac4872acf
Phiko <phgu03@gmail.com>
2026-02-24 13:12:04 +0100

    🐞 fix(auth): add eslint directives for handling unsafe return and member access in getClientIp utility

--- backend/src/api/controllers/auth.controller.ts
+++ backend/src/api/controllers/auth.controller.ts
@@ -15,6 +15,7 @@ import * as AuthService from "../services/auth.service";
 // Utility function to get the client's IP address
 // eslint-disable-next-line func-style
 const getClientIp = (req: any): string => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
   return req.headers["x-forwarded-for"] || "";
 };
 
