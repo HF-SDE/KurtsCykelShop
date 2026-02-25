878e415f65c9970cac64da867e331236e9315430
Phiko <phgu03@gmail.com>
2026-02-25 12:57:59 +0100

    ✨ feat(layout): add edit role screen with save button in header

--- app/app/(auth)/(tabs)/admin/roles/_layout.tsx
+++ app/app/(auth)/(tabs)/admin/roles/_layout.tsx
@@ -1,6 +1,9 @@
 import { BackButtonLayout } from "@components/back-button";
+import { Button, ButtonIcon } from "@components/ui/button";
 import { Text } from "@components/ui/text";
 import { Stack } from "expo-router";
+import { Save } from "lucide-react-native";
+
 import RoleProvider from "./ctx";
 
 export const unstable_settings = {
@@ -13,7 +16,17 @@ export default function StorageLayout() {
       <BackButtonLayout>
         <Stack.Screen name="roles" options={{ headerShown: true, headerTitle: () => <Text size="2xl">Roller</Text> }} />
         <Stack.Screen name="new" options={{ headerTitle: () => <Text size="2xl">Ny rolle</Text> }} />
-        <Stack.Screen name="[id]/edit" options={{ headerTitle: () => <Text size="2xl">Rediger rolle</Text> }} />
+        <Stack.Screen
+          name="[id]/edit"
+          options={{
+            headerTitle: () => <Text size="2xl">Rediger rolle</Text>,
+            headerRight: () => (
+              <Button variant="link" action="secondary" className="px-0">
+                <ButtonIcon as={Save} size="xl" />
+              </Button>
+            ),
+          }}
+        />
       </BackButtonLayout>
     </RoleProvider>
   );
