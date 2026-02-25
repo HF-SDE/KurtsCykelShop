ed1bbae185c279c114b8a6ded7b6eb89735ccee7
Nima Badiei <nimabadiei@gmail.com>
2026-02-25 11:09:56 +0100

    Fixed padding at bottom of cases index view

--- app/app/(auth)/(tabs)/case/index.tsx
+++ app/app/(auth)/(tabs)/case/index.tsx
@@ -1,12 +1,10 @@
-import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { CasesSection } from "@/components/cases/casesSection";
-import { Text } from "@/components/ui/text";
 
 export default function Tab1() {
   return (
-    <SafeAreaView className="bg-background-0 flex-1 p-4">
+    <SafeAreaView className="bg-background-0 flex-1 p-4" edges={{ top: "additive" }}>
       <CasesSection />
     </SafeAreaView>
   );
