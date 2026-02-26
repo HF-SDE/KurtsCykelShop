fcc2680445d578630bf3ee714e722ade57191f53
Phiko <phgu03@gmail.com>
2026-02-26 14:22:01 +0100

    ✨ feat(layout): add users screen to AdminLayout for user management

--- app/app/(auth)/(tabs)/admin/_layout.tsx
+++ app/app/(auth)/(tabs)/admin/_layout.tsx
@@ -5,6 +5,7 @@ export default function AdminLayout() {
   return (
     <BackButtonLayout>
       <Stack.Screen name="roles" options={{ headerShown: false }} />
+      <Stack.Screen name="users" options={{ headerShown: false }} />
     </BackButtonLayout>
   );
 }
