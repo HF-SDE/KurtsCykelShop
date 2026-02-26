b9d745f8a832a6ef0a6da1604600edb634029b79
Phiko <phgu03@gmail.com>
2026-02-26 10:58:09 +0100

    ✨ feat(permission): wrap buttons in CheckPageAccess for role and user management

--- app/app/(auth)/(tabs)/admin/index.tsx
+++ app/app/(auth)/(tabs)/admin/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Button, ButtonGroup, ButtonIcon, ButtonText } from "@/components/ui/button";
 
+import CheckPageAccess from "@components/check-page-access";
 import { Center } from "@components/ui/center";
 import { LockIcon, UserShieldIcon } from "@components/ui/icon";
 import { useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Admin() {
   const router = useRouter();
@@ -17,20 +18,25 @@ export default function Admin() {
 
   function handleUsersPress() {
     // Handle users button press
+    router.navigate("/admin/users");
   }
 
   return (
     <SafeAreaView className="bg-background-0 flex-1">
       <Center className="bg-background-0 flex-1">
         <ButtonGroup>
-          <Button action="primary" variant="solid" size="3xl" onPress={handleRolesPress}>
-            <ButtonText size="2xl">Roles</ButtonText>
-            <ButtonIcon as={LockIcon} size="4xl" />
-          </Button>
-          <Button action="primary" variant="solid" size="3xl" onPress={handleUsersPress}>
-            <ButtonText size="2xl">Users</ButtonText>
-            <ButtonIcon as={UserShieldIcon} size="4xl" />
-          </Button>
+          <CheckPageAccess pageName="RolesPage">
+            <Button action="primary" variant="solid" size="3xl" onPress={handleRolesPress}>
+              <ButtonText size="2xl">Roles</ButtonText>
+              <ButtonIcon as={LockIcon} size="4xl" />
+            </Button>
+          </CheckPageAccess>
+          <CheckPageAccess pageName="UsersPage">
+            <Button action="primary" variant="solid" size="3xl" onPress={handleUsersPress}>
+              <ButtonText size="2xl">Users</ButtonText>
+              <ButtonIcon as={UserShieldIcon} size="4xl" />
+            </Button>
+          </CheckPageAccess>
         </ButtonGroup>
       </Center>
     </SafeAreaView>
