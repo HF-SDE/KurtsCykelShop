1324d89f03baa141c68445f85c566e59554b04fa
Phiko <phgu03@gmail.com>
2026-02-26 11:57:47 +0100

    ✨ feat(edit-role): remove unused Plus icon import from EditRolePage

--- app/app/(auth)/(tabs)/admin/roles/[id]/edit.tsx
+++ app/app/(auth)/(tabs)/admin/roles/[id]/edit.tsx
@@ -16,7 +16,7 @@ import { Text } from "@components/ui/text";
 import { Textarea, TextareaInput } from "@components/ui/textarea";
 import { Toast, ToastDescription, ToastTitle, useToast } from "@components/ui/toast";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import { ListFilter, Plus, Save } from "lucide-react-native";
+import { ListFilter, Save } from "lucide-react-native";
 
 import { useRole } from "../ctx";
 
