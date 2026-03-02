c093a8474284bd9017370a140413adb642b2d5ba
Nima Badiei <nimabadiei@gmail.com>
2026-03-02 08:43:05 +0100

    ✨ feat(item-statuses): add JWT verification middleware and clean up route imports

--- backend/src/api/routes/itemStatuses.routes.ts
+++ backend/src/api/routes/itemStatuses.routes.ts
@@ -1,9 +1,12 @@
 import { getAll, getAllPaginated } from "@controllers/itemStatus.controller";
+import { verifyJWT } from "@middlewares/authenticate.mw";
 import { Router } from "express";
 
 const router = Router();
 
+router.use("/", verifyJWT);
+
 router.get("/paginated", getAllPaginated);
-router.get(["/", "/:id"], getAll);
+router.get("/", getAll);
 
 export { router as itemStatusRoutes };
